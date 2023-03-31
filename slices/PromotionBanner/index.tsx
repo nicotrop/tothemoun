e5748977ff0a2bd37b27035b78374f657b695882
Nicolas Troplent <nicolas.troplent@gmail.com>
2023-03-31 16:06:14 -0400

    fixed deploy error

--- slices/PromotionBanner/index.tsx
+++ slices/PromotionBanner/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { SliceComponentProps } from "@prismicio/react";
-import useMediaQuery from "@/utils/hooks";
 import { Wrapper } from "@/components/Layout";
 import { PromotionBannerSlice } from "../../.slicemachine/prismicio";
 import { PrismicNextImage } from "@prismicio/next";
