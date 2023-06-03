4fc840b906758d2ebedf32daa7a71a7e23929ab4
Nicolas Troplent <nicolas.troplent@gmail.com>
2023-06-03 20:52:04 +0200

    ready to deploy

--- src/pages/previewslices.tsx
+++ src/pages/previewslices.tsx
@@ -2,31 +2,18 @@ import { Layout } from "@/components/global";
 import { HeroVideo } from "@/components/heroComponents";
 import { OneArticleBlock } from "@/components/oneArticle";
 import { SEOsection } from "@/components/seoSection";
-import { TopStories } from "@/components/topStories";
-import { CollectionCarousel } from "@/components/collectionCarousel";
 import SquareCardCarousel from "@/components/squareCardsCarousel";
-import { InterceptImprovedGrid } from "@/components/mixedGridImproved";
 import { PhotoBanner } from "@/components/photoBanner";
 import { PinterestGrid } from "@/components/pinterestGrid";
 import { MarqueeBannerComp } from "@/components/marqueeBanner";
-// import { VanityHero } from "@/components/vanityHero";
-// import { CollectionGrid } from "@/components/collectionGrid";
 
 export default function PreviewSlices() {
   return (
     <Layout>
       <HeroVideo />
-      {/* <VanityHero /> */}
-      {/* <CollectionGrid /> */}
-      {/* <TopStories /> */}
-      {/* <CollectionCarousel /> */}
-      {/* <InterceptImprovedGrid /> */}
       <OneArticleBlock />
-      {/* <InterceptImprovedGrid /> */}
       <SquareCardCarousel />
-      {/* <InterceptImprovedGrid /> */}
       <PhotoBanner />
-      {/* <InterceptImprovedGrid /> */}
       <MarqueeBannerComp />
       <SEOsection />
       <PinterestGrid />
--- src/pages/que-faire-en-guadeloupe/[[...uid]].tsx
+++ src/pages/que-faire-en-guadeloupe/[[...uid]].tsx
@@ -29,8 +29,8 @@ export async function getStaticProps({
   const uidPage = params?.uid?.slice(-1)[0] || "que-faire-en-guadeloupe";
 
   // Initialize category and page
-  let category;
-  let categoryArr;
+  // let category;
+  // let categoryArr;
   let page;
 
   type justCategoryType = boolean | null;
@@ -40,21 +40,21 @@ export async function getStaticProps({
 
   let justCategory: justCategoryType = mightJustCategory;
 
-  if (mightJustCategory) {
-    category = await client.getAllByType("blog_collection");
-    categoryArr = category?.map((elem) => elem.uid);
-    const isInCategoryArr = categoryArr?.includes(uidPage);
-    justCategory = isInCategoryArr;
-  }
+  // if (mightJustCategory) {
+  //   category = await client.getAllByType("blog_collection");
+  //   categoryArr = category?.map((elem) => elem.uid);
+  //   const isInCategoryArr = categoryArr?.includes(uidPage);
+  //   justCategory = isInCategoryArr;
+  // }
 
   // If page uid is not "que-faire-en-guadeloupe" and justCategory is false, fetch blog_post by uid
   if (uidPage !== "que-faire-en-guadeloupe" && justCategory === false) {
     console.log("type blog post");
     page = await client.getByUID("blog_post", uidPage);
     //lse if page uid is not "que-faire-en-guadeloupe" and justCategory is true, fetch blog_collection by uid
-  } else if (uidPage !== "que-faire-en-guadeloupe" && justCategory === true) {
-    console.log("type blog collection");
-    page = await client.getByUID("blog_collection", uidPage);
+    // } else if (uidPage !== "que-faire-en-guadeloupe" && justCategory === true) {
+    //   console.log("type blog collection");
+    //   page = await client.getByUID("blog_collection", uidPage);
   } else {
     console.log("type collection");
     // Else fetch "que-faire-en-guadeloupe" collection by uid
